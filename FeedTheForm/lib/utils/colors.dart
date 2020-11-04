4ce0ac4a54046f46f27008ad7f0b2d645affd7fe
jccmartins <jccmvcd@gmail.com>
2020-11-04 16:57:18 +0000

    added some colors

--- FeedTheForm/lib/utils/colors.dart
+++ FeedTheForm/lib/utils/colors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const Color darkGray1 = Color.fromRGBO(18, 18, 18, 1);
+const Color darkGray2 = Color.fromRGBO(48, 48, 48, 1);
+
 ThemeData getTheme(BuildContext context) {
   return ThemeData();
 }
