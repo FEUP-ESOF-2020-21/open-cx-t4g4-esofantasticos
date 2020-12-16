44f62bb5a114d189a834515dc79fc6b5cafed469
Zé Pedro <nepsname@gmail.com>
2020-12-16 23:27:49 +0000

    sorteio de vencedor

--- FeedTheForm/lib/controllers/MyController.dart
+++ FeedTheForm/lib/controllers/MyController.dart
@@ -1,12 +1,35 @@
 import 'DatabaseController.dart';
+import "dart:math";
 
 class MyController implements DatabaseController {
   String currentUser;
+  Map<String,  Map<String, double>> ratingsPerLecture;
+  Map<int, String> winnersPerLecture;
 
-  MyController(
-      String currentUser,
-      ) {
+  MyController(String currentUser) {
     this.currentUser = currentUser;
+    ratingsPerLecture = new Map<String,  Map<String, double>>();
+    winnersPerLecture = new Map<int, String>();
+
+    winnersPerLecture.update(1, (_) => "To be announced", ifAbsent: () => "To be announced");
+    winnersPerLecture.update(2, (_) => "To be announced", ifAbsent: () => "To be announced");
+    winnersPerLecture.update(3, (_) => "To be announced", ifAbsent: () => "To be announced");
+
+    Map<String, double> ratingsFirstLecture = new Map<String, double>();
+    Map<String, double> ratingsSecondLecture = new Map<String, double>();
+    Map<String, double> ratingsThirdLecture = new Map<String, double>();
+    ratingsFirstLecture.update("user1", (_) => 2, ifAbsent: () => 2);
+    ratingsFirstLecture.update("user2", (_) => 1, ifAbsent: () => 1);
+    ratingsFirstLecture.update("user3", (_) => 5, ifAbsent: () => 5);
+    ratingsSecondLecture.update("user1", (_) => 5, ifAbsent: () => 5);
+    ratingsSecondLecture.update("user2", (_) => 4, ifAbsent: () => 4);
+    ratingsSecondLecture.update("user3", (_) => 5, ifAbsent: () => 5);
+    ratingsThirdLecture.update("user1", (_) => 2, ifAbsent: () => 2);
+    ratingsThirdLecture.update("user2", (_) => 1, ifAbsent: () => 1);
+    ratingsThirdLecture.update("user3", (_) => 3, ifAbsent: () => 3);
+    ratingsPerLecture.update("Applying AI to Real World Use Cases", (_) => ratingsFirstLecture, ifAbsent: () => ratingsFirstLecture);
+    ratingsPerLecture.update("Targeting Metabolism to Treat the Aging Brain", (_) => ratingsSecondLecture, ifAbsent: () => ratingsSecondLecture);
+    ratingsPerLecture.update("Machine design innovation through technology and education", (_) => ratingsThirdLecture, ifAbsent: () => ratingsThirdLecture);
   }
 
   @override
@@ -18,5 +41,42 @@ class MyController implements DatabaseController {
   @override
   String getCurrentUser() {
     return currentUser;
+  }
+
+  String getPrizeWinner(prizeID) {
+    return(winnersPerLecture[prizeID]);
+  }
+
+  void addRating(String lecture, String user, double rating) {
+    if(ratingsPerLecture.keys.contains(lecture)) {
+      ratingsPerLecture.forEach((key, ratings) {
+        if(lecture == key) {
+          ratings.update(user, (_) => rating, ifAbsent: () => rating);
+          ratingsPerLecture.update(lecture, (_) => ratings);
+        }
+      });
+    } else {
+      Map<String, double> ratings;
+      ratings.update(user, (_) => rating, ifAbsent: () => rating);
+      ratingsPerLecture.update(lecture, (_) => ratings, ifAbsent: () => ratings);
+    }
+  }
+
+  String generateWinner(int prizeID) {
+    String randomWinner;
+    List<String> participants = new List<String>();
+    final _random = new Random();
+
+    if(ratingsPerLecture.isNotEmpty) {
+      ratingsPerLecture.forEach((_, ratings) {
+        ratings.forEach((user, _) {
+          participants.add(user);
+        });
+      });
+
+      randomWinner = participants[_random.nextInt(participants.length)];
+      winnersPerLecture.update(prizeID, (_) => randomWinner + "!");
+      return randomWinner;
+    } else return null;
   }
 }
--- FeedTheForm/lib/views/Pages/PrizesPageView/prize_page_view.dart
+++ FeedTheForm/lib/views/Pages/PrizesPageView/prize_page_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:FeedTheForm/controllers/MyController.dart';
 import 'package:FeedTheForm/widgets/icon_and_info_row.dart';
 import 'package:flutter/material.dart';
@@ -7,8 +9,18 @@ class PrizePageView extends StatelessWidget {
 
   PrizePageView(this.dbController);
 
+  TextEditingController prize1 = new TextEditingController();
+  TextEditingController prize2 = new TextEditingController();
+  TextEditingController prize3 = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    int prizeID1 = 1;
+    int prizeID2 = 2;
+    int prizeID3 = 3;
+    prize1.text = "Winner: " + dbController.getPrizeWinner(prizeID1);
+    prize2.text = "Winner: " + dbController.getPrizeWinner(prizeID2);
+    prize3.text = "Winner: " + dbController.getPrizeWinner(prizeID3);
     return Scaffold(
         backgroundColor: Colors.black54,
         body: SingleChildScrollView(
@@ -41,12 +53,38 @@ class PrizePageView extends StatelessWidget {
                     Row(
                       children: <Widget>[
                         Expanded(
-                          child: Text(
-                            "Winner: To be announced",
+                          child: TextField(
+                            controller: prize1,
                             style: TextStyle(color: Colors.white, fontSize: 20),
                             textAlign: TextAlign.left,
                           ),
-                        )
+                        ),
+                        if (dbController.isAdmin()) Align(
+                          alignment: Alignment.centerRight,
+                          child: FlatButton(
+                            onPressed: () {
+                              String winner = dbController.generateWinner(prizeID1);
+                              if(winner != null) prize1.text = "Winner: " + winner + "!";
+                              else {
+                                prize1.text = "Insufficient ratings!";
+                                Future.delayed(const Duration(seconds: 2), () {
+                                  prize1.text = "Winner: To be announced";
+                                });
+                              }
+                            },
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(5)),
+                            child: Text(
+                              "Generate winner",
+                              style: TextStyle(
+                                color: Colors.blue[600],
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                     SizedBox(height: 30),
@@ -75,12 +113,38 @@ class PrizePageView extends StatelessWidget {
                     Row(
                       children: <Widget>[
                         Expanded(
-                          child: Text(
-                            "Winner: To be announced",
+                          child: TextField(
+                            controller: prize2,
                             style: TextStyle(color: Colors.white, fontSize: 20),
                             textAlign: TextAlign.left,
                           ),
-                        )
+                        ),
+                        if (dbController.isAdmin()) Align(
+                          alignment: Alignment.centerRight,
+                          child: FlatButton(
+                            onPressed: () {
+                              String winner = dbController.generateWinner(prizeID2);
+                              if(winner != null) prize2.text = "Winner: " + winner + "!";
+                              else {
+                                prize2.text = "Insufficient ratings!";
+                                Future.delayed(const Duration(seconds: 2), () {
+                                  prize2.text = "Winner: To be announced";
+                                });
+                              }
+                            },
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(5)),
+                            child: Text(
+                              "Generate winner",
+                              style: TextStyle(
+                                color: Colors.blue[600],
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                     SizedBox(height: 30),
@@ -109,12 +173,38 @@ class PrizePageView extends StatelessWidget {
                     Row(
                       children: <Widget>[
                         Expanded(
-                          child: Text(
-                            "Winner: To be announced",
+                          child: TextField(
+                            controller: prize3,
                             style: TextStyle(color: Colors.white, fontSize: 20),
                             textAlign: TextAlign.left,
                           ),
-                        )
+                        ),
+                        if (dbController.isAdmin()) Align(
+                          alignment: Alignment.centerRight,
+                          child: FlatButton(
+                            onPressed: () {
+                              String winner = dbController.generateWinner(prizeID3);
+                              if(winner != null) prize3.text = "Winner: " + winner + "!";
+                              else {
+                                prize3.text = "Insufficient ratings!";
+                                Future.delayed(const Duration(seconds: 2), () {
+                                  prize3.text = "Winner: To be announced";
+                                });
+                              }
+                            },
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                                color: Colors.blue,
+                                width: 1,
+                                style: BorderStyle.solid
+                            ), borderRadius: BorderRadius.circular(5)),
+                            child: Text(
+                              "Generate winner",
+                              style: TextStyle(
+                                color: Colors.blue[600],
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ],
